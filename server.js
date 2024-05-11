f2d5551d644f997d5066ba19f1fa3eed143d0ab7
RomanNestorko <104514561+RomanXz92@users.noreply.github.com>
2024-05-11 14:22:48 +0200

    first

---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config(); // Завантаження змінних середовища з .env файлу
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+app.use(cors());
+
+app.get("/weather", async (req, res) => {
+  const { lat, lon } = req.query;
+
+  try {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    const response = await axios.get(apiUrl);
+    const data = response.data;
+
+    const city = data.name;
+    const country = data.sys.country;
+    const location = `${city}, ${country}`;
+
+    res.json({
+      ...data,
+      location: location,
+    });
+  } catch (error) {
+    console.error("Помилка отримання погоди:", error);
+    res.status(500).json({ error: "Помилка отримання погоди" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущено на порті ${PORT}`);
+});
