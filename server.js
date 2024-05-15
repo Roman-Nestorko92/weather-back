3380e054ab347e697c3f9b0e27544759e8d31771
RomanNestorko <104514561+RomanXz92@users.noreply.github.com>
2024-05-15 22:18:48 +0200

    comit

--- server.js
+++ server.js
@@ -31,6 +31,31 @@ app.get("/weather", async (req, res) => {
   }
 });
 
+app.get("/weather/city", async (req, res) => {
+  const { q } = req.query;
+
+  try {
+    // Виконати запит до погодового API за допомогою параметра 'q' (місто)
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&appid=${apiKey}`;
+    const response = await axios.get(apiUrl);
+    const data = response.data;
+
+    // Отримати інформацію про місто і країну з отриманих даних
+    const city = data.name;
+    const country = data.sys.country;
+    const location = `${city}, ${country}`;
+
+    // Відправити відформатовані дані на клієнт
+    res.json({
+      ...data,
+      location: location,
+    });
+  } catch (error) {
+    console.error("Помилка отримання погоди:", error);
+    res.status(500).json({ error: "Помилка отримання погоди" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Сервер запущено на порті ${PORT}`);
 });
